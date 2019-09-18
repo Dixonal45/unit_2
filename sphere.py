b2e4938b5dffae2dc33aa8809ad799553868131f
allison <allison.dixon@ssfs.org>
2019-09-18 14:40:17 -0400

    Did almost three problems

---
+++ sphere.py
@@ -0,0 +1,12 @@
+# problem 1
+volume_of_sphere = (4 / 3) * (3.14) * (5 ** 3)
+print("The volume of the sphere is", volume_of_sphere)
+
+# problem 2
+num_books = 60
+book_cost = ((0.6 * 24.95) * num_books) + 3 + (0.75 * num_books - 1)
+print("The total wholesale cost for the books are", book_cost)
+
+# problem 3
+running_time = (((2 * 8.25) + (3 * 7.12) + (6 * 60 + 52)) / 60)
+print("The time you will be home for breakfast is", running_time)
