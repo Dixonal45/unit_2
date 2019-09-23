96a1b21439a81554a6505bb5beae85ea52700286
allison <allison.dixon@ssfs.org>
2019-09-23 10:14:47 -0400

    started unit 2 project: asking several questions to user.

---
+++ unit2_one.py
@@ -0,0 +1,23 @@
+# by Allison Dixon
+# last updated September 23, 2019
+# unit two assignment option one
+
+name = input("What is your name?")
+print("I guess it's nice to meet you", name, "! My name is The Banished Prince Zuko of the Fire Nation.")
+
+location = input("Where are you from?")
+print("I've heard", location, "is an okay place. Maybe I'll go there once I get my honor back.")
+
+number = int(input("What is your favorite number?"))
+print("Your favorite number", number, "is", number/8, "times bigger than my favorite number 8!")
+
+car = input("What is your dream car?")
+print("Oh my goodness, I've always wanted a", car, "too, but all I have is a ship.")
+
+cost = float(input("How much does that car cost?"))
+print("That's pretty expensive and I'm broke after Uncle spent all our money buying a new lotus tile.")
+
+years = int(input("How many years would you take a loan for to pay for the " + car + "?"))
+
+interest = float(input("What is the annual interest rate (percent) for the " + car + "?"))
+
