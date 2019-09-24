ce687dab7409511cf40e0507f847b4b473bb93da
allison <allison.dixon@ssfs.org>
2019-09-24 11:19:44 -0400

    Did some more problems

--- sphere.py
+++ sphere.py
@@ -2,11 +2,23 @@
 volume_of_sphere = (4 / 3) * (3.14) * (5 ** 3)
 print("The volume of the sphere is", volume_of_sphere)
 
-# problem 2
+# problem
 num_books = 60
 book_cost = ((0.6 * 24.95) * num_books) + 3 + (0.75 * num_books - 1)
 print("The total wholesale cost for the books are", book_cost)
 
-# problem 3
+# problem  is not working
 running_time = (((2 * 8.25) + (3 * 7.12) + (6 * 60 + 52)) / 60)
 print("The time you will be home for breakfast is", running_time)
+
+# problem 2 finds area of rectangle
+length = 6
+width = 7
+area_of_rectangle = length * width
+print(area_of_rectangle)
+
+# problem 3 finds area of triangle
+base = input("What is the base of the triangle?")
+height = input("What is the height of the triangle?")
+area_of_triangle = 0.5 * float(base * height)
+print(area_of_triangle)
